031f5f68770dfb0b0dc60967a738c1cd45caf787
rivaldomrtd <rivaldomrtd@gmail.com>
2021-11-05 08:45:04 +0700

    Update home.blade.php

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <style>
     body,h1 {font-family: "Raleway", sans-serif}
@@ -18,8 +22,23 @@
     }
     </style>
 <body>
+
     <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
         <div class="w3-display-topleft w3-padding-large w3-xlarge">Valdoos</div>
+        <div class="w3-display-topright w3-padding-large w3-xlarge">
+            <button type="button" class="btn btn-success">
+                <a style="color: white" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </button>
+
+        </div>
+
     <div class="w3-display-middle">
         <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
         <hr class="w3-border-grey" style="margin:auto;width:40%">
