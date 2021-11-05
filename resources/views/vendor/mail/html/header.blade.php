23136d74608edb779d7cec85fc0f94c4b8f13d13
rivaldomrtd <rivaldomrtd@gmail.com>
2021-11-06 04:26:06 +0700

    EmailCustom

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Valdoos')
+<img src="https://avatars.githubusercontent.com/u/93493489?s=200&v=4" class="logo" alt="Laravel Logo">
+@else
+@endif
+</a>
+</td>
+</tr>
