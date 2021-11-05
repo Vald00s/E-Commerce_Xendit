ef2163f621012d9d2fd4340b03fe31e35970dc41
rivaldomrtd <rivaldomrtd@gmail.com>
2021-11-05 08:21:58 +0700

    user soon

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,23 +1,34 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard {{ $user->name }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <p><strong>Selamat datang USER wkwkwk!</strong> Anda telah melakukan login sebagai {{ $user->role }}</p>
-                </div>
-            </div>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Coming SOON</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+</head>
+<style>
+    body,h1 {font-family: "Raleway", sans-serif}
+    body, html {height: 100%}
+    .bgimg {
+    background-image: url('https://raw.githubusercontent.com/rivaldomrtd/rivaldomrtd/main/3.png');
+    min-height: 100%;
+    background-position: center;
+    background-size: cover;
+    }
+    </style>
+<body>
+    <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+        <div class="w3-display-topleft w3-padding-large w3-xlarge">Valdoos</div>
+    <div class="w3-display-middle">
+        <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
+        <hr class="w3-border-grey" style="margin:auto;width:40%">
+        <p class="w3-large w3-center">82 days left</p>
     </div>
-</div>
-@endsection
+    <div class="w3-display-bottomleft w3-padding-large">Powered by
+        <a href="https://github.com/Vald00s" target="_blank">vald00s.org</a>
+    </div>
+    </div>
+
+</body>
+</html>
