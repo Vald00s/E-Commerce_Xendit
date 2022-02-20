e8d53ccb44aa361dc7455b80655c1e8a8ce61ce0
Rivaldo Martadinata <rivaldomrtd@gmail.com>
2022-02-20 20:25:21 +0700

    00s

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,84 +1,88 @@
-
 @extends('layouts.app')
 @section('content')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
-    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-</head>
-<div class="main">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+        <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+    </head>
+    <div class="main">
 
-    <!-- Sign up form -->
-    <section class="signup">
-        <div class="container">
-            <div class="signup-content">
-                <div class="signup-form">
-                    <h2 class="form-title">Sign up</h2>
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="name">
-                                <i class="zmdi zmdi-account material-icons-name"></i></label>
-                            <input id="name" type="text"  @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="Username" >
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container2">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Sign up</h2>
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">
+                                    <i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input id="name" type="text" @error('name') is-invalid @enderror" name="name"
+                                    value="{{ old('name') }}" required autocomplete="name" placeholder="Username">
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="email"><i class="zmdi zmdi-email"></i></label>
-                            <input id="email" type="email"  @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Alamat Email">
+                            </div>
+                            <div class="form-group">
+                                <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                <input id="email" type="email" @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" placeholder="Alamat Email">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
 
-                        </div>
-                        <div class="form-group">
-                            <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                            <input id="password" type="password"  @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                            <input id="password-confirm" type="password"  name="password_confirmation" required autocomplete="new-password" placeholder="Repeat Password">
+                            </div>
+                            <div class="form-group">
+                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input id="password" type="password" @error('password') is-invalid @enderror"
+                                    name="password" required autocomplete="new-password" placeholder="Password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                        </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input id="password-confirm" type="password" name="password_confirmation" required
+                                    autocomplete="new-password" placeholder="Repeat Password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
 
-                        <div class="form-group">
-                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
-                        </div>
-                        <div class="form-group form-button">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Register') }}
+                            <div class="form-group">
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all
+                                    statements in <a href="#" class="term-service">Terms of service</a></label>
+                            </div>
+                            <div class="form-group form-button">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
 
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                <div class="signup-image">
-                    <figure><img src="https://avatars.githubusercontent.com/u/93493489?s=200&v=4" alt="sing up image"></figure>
-                    <a href="{{ route('login') }}" class="signup-image-link">I am already member</a>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="https://avatars.githubusercontent.com/u/93493489?s=200&v=4" alt="sing up image">
+                        </figure>
+                        <a href="{{ route('login') }}" class="signup-image-link">I am already member</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
-<script src="../js/jquery.min.js"></script>
-<script src="../js/main.js"></script>
-
+        </section>
+    </div>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/main.js"></script>
 @endsection
